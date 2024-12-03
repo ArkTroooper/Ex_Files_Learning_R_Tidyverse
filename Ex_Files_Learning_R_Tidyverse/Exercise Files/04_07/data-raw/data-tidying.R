8e980533cd885367a89f646f4c38e02aa249d742
Aaron Krishna <aaron.krishna@laysholdings.co.za>
2024-12-03 09:31:34 +0200

    removing na columns

--- Ex_Files_Learning_R_Tidyverse/Exercise Files/04_07/data-raw/data-tidying.R
+++ Ex_Files_Learning_R_Tidyverse/Exercise Files/04_07/data-raw/data-tidying.R
@@ -7,6 +7,11 @@ colnames(transport_data) <- tolower(make.names(colnames(transport_data)))
 
 
 transport_data %>%
-  separate(sender.location, c("sender..country","sender.city"),sep = ",")
+  separate(sender.location, c("sender.country","sender.city"),sep = ",",extra = "merge") %>% 
+  separate(receiver.location,
+           c("receiver.country","receiver.city"),sep = ",",extra ="merge")
 
+transport_data %>%
+  select(sender.location) %>%
+  slice(c(316,317, 318, 319, 320, 321, 322, 323, 324, 325, 326,327, 328, 329,330, 331))
 write_csv(transport_data, "data/transport_data.csv")
